f7b9f2a3732c324e321dcadf68d3ad2b82e46a42
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2023-09-14 19:34:15 +0200

    feat: initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 120,
+    arrowParens: 'avoid'
+};
